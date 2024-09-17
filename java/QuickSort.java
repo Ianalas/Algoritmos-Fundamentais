3c4e85872a8dfbec9890c27b112c2cd337790593
Ian Alas <136917216+Ianalas@users.noreply.github.com>
2024-09-17 18:19:17 -0300

    Update QuickSort.java

--- java/QuickSort.java
+++ java/QuickSort.java
@@ -5,32 +5,27 @@ public class QuickSort{
   
   public void DoQuickSort(){
     
-      System.out.println("Digite o tamanho do vetor: ");
-      int n = prompt.nextInt();
-      int[] vetor = new int[n];
-      for(int i = 0; i < n; i++){
-        System.out.println("Digite o valor da "+ i + "º pos.");
-        vetor[i] = prompt.nextInt();
-      }
-    
-      System.out.println("Before QuickSort");
-      for (int i = 0; i < n; i++){
-        System.out.println(vetor[i]);
-      }
+    System.out.println("Digite o tamanho do vetor: ");
+    int n = prompt.nextInt();
+    int[] vetor = new int[n];
+    for(int i = 0; i < n; i++){
+      System.out.println("Digite o valor da "+ i + "º pos.");
+      vetor[i] = prompt.nextInt();
+    }
 
-      for (int i = 0; i < (n-1); i++){
-        if(vetor[i] > vetor[i+1]){
+    for (int j = 0; j < n - 1; j++) {
+      for (int i = 0; i < n - 1 - j; i++) {
+        if (vetor[i] > vetor[i + 1]) {
           int aux = vetor[i];
-          vetor[i] = vetor[i+1];
-          vetor[i+1] = aux;
+          vetor[i] = vetor[i + 1];
+          vetor[i + 1] = aux;
         }
       }
+    }
 
-      System.out.println("After QuickSort");
-      for (int i = 0; i < n; i++){
-        System.out.println(vetor[i]);
-      }
+    System.out.println("After QuickSort");
+    for (int i = 0; i < n; i++){
+      System.out.println(vetor[i]);
+    }
   }
-
-  
 }
